e8d7ce7cae3fb503eed3b510a937497e2d0e4fb7
hayden coffing <coffinghayden@gmail.com>
2023-12-03 14:58:31 -0800

    an inital prototype! added: - a simple, yet ugly, gui - no documentation :) - emotion recognition - facial recogntion complete with training! more to come! because it has to!

--- main.py
+++ main.py
@@ -1,24 +1,133 @@
-import torch.cuda
-from facial_emotion_recognition import EmotionRecognition
-import face_recognition as fr
-import cv2 as cv
-print(torch.cuda.is_available())
-er :EmotionRecognition = EmotionRecognition(device='gpu', gpu_id=0)
+import cv2
+import tkinter as tk
+from PIL import Image, ImageTk
+from fer import FER
+import face_recognition
+import os
 
-cam = cv.VideoCapture(0)
+class EmotionRecognitionDemo:
+    def __init__(self, window, window_title):
+        # create a window and a webcam feed
+        self.window = window
+        self.window.title(window_title)
 
-success, frame = cam.read()
+        # webcam is usually 0
+        self.cap = cv2.VideoCapture(0)
 
-frame = er.recognise_emotion(frame, return_type='BGR')
+        self.canvas = tk.Canvas(window, width=self.cap.get(3), height=self.cap.get(4))
+        self.canvas.pack()
 
-cv.imshow("frame", frame)
+        # create labels
+        self.emotion_label = tk.Label(window, text="Detected Emotion: None", font=("Helvetica", 16))
+        self.name_label = tk.Label(window, text="Recognized Face: ",font=("Helvetica", 16))
+        self.name_label.pack(pady=10)
+        self.emotion_label.pack(pady=10)
 
-while True:
-    key = cv.waitKey(10)
-    if key & 0xff == 27:
-        break
-def main():
-    pass
+        # name entry field
+        self.name_entry = tk.Entry(window)
+        self.name_entry.pack(pady=10)
 
-if __name__ == '__main__':
-    main()
+        # add buttons
+        self.train_button = tk.Button(window, text="Train Face", command=self.train_face)
+        self.train_button.pack(pady=10)
+
+        self.recognize_button = tk.Button(window, text="Recognize Face", command=self.recognize_face)
+        self.recognize_button.pack(pady=10)
+
+        # create fer instance
+        self.fer = FER()
+
+        self.known_faces = []
+        self.face_encodings = []
+        self.face_names = []
+        self.training_mode = False
+
+
+        self.update()
+
+    def update(self):
+        # grab a single frame
+        ret, frame = self.cap.read()
+
+        if ret:
+            # detect emotions on the face
+            emotions = self.fer.detect_emotions(frame)
+
+            if emotions:
+                # get highest correlation emotion
+                dominant_emotion = max(emotions[0]["emotions"].items(), key=lambda x: x[1])[0]
+
+                # update emotion label in the gui
+                self.emotion_label.config(text=f"Detected Emotion: {dominant_emotion}")
+
+                # have to convert the image to a pil image to display it :(
+                img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                img = Image.fromarray(img)
+                img = ImageTk.PhotoImage(image=img)
+
+                # update image
+                self.canvas.img = img
+                self.canvas.create_image(0, 0, anchor=tk.NW, image=img)
+
+        # grab next image after 10 ms
+        self.window.after(10, self.update)
+
+    def train_face(self):
+        # switch to train mode
+        self.training_mode = True
+
+        # grab a single frame
+        ret, frame = self.cap.read()
+
+        if ret:
+            # find all faces and encoding in the frame
+            face_locations = face_recognition.face_locations(frame)
+            face_encodings = face_recognition.face_encodings(frame, face_locations)
+
+            # if one of the faces is detected...
+            #TODO: check if the detected face isnt in the dataset!
+            if face_encodings:
+                # assuming theres only one face in frame.
+                face_encoding = face_encodings[0]
+
+                name = self.name_entry.get()
+
+                # add face and name to the lists.
+                self.face_encodings.append(face_encoding)
+                self.face_names.append(name)
+
+        self.training_mode = False
+
+    def recognize_face(self):
+        # recognizing face isnt a training mode
+        self.training_mode = False
+
+        # grab a frame.
+        ret, frame = self.cap.read()
+
+        if ret:
+            # grab all faces and encodings
+            face_locations = face_recognition.face_locations(frame)
+            face_encodings = face_recognition.face_encodings(frame, face_locations)
+
+            # check face against all faces we know.
+            for face_encoding in face_encodings:
+                matches = face_recognition.compare_faces(self.face_encodings, face_encoding)
+                name = "Unknown"
+
+                if True in matches:
+                    first_match_index = matches.index(True)
+                    name = self.face_names[first_match_index]
+                    self.name_label.config(text=f"Detected Face: {name}")
+
+                print(f"Recognized face: {name}")
+
+    def close_app(self):
+        # close
+        self.cap.release()
+        self.window.destroy()
+
+# create tkinter and run the app
+root = tk.Tk()
+app = EmotionRecognitionDemo(root, "Project Awesome")
+root.mainloop()
