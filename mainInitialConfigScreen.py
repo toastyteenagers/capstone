23c029cd52dbcfa9eb8b53712f7b80a153e242a5
Roohan Amin <roohan.amin@gmail.com>
2024-03-07 07:13:31 -0800

    more screens, better looking screens

--- mainInitialConfigScreen.py
+++ mainInitialConfigScreen.py
@@ -1,11 +1,37 @@
 from PyQt6 import QtCore, QtGui, QtWidgets
-from initialConfigScreenOne import Ui_InitConfScreenOne
 
+
+class HoverButton(QtWidgets.QPushButton):
+    def __init__(self, default_text, hover_text, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.default_text = default_text
+        self.hover_text = hover_text
+        # Set the default text initially
+        self.setText(self.default_text)
+        # Default style
+        self.default_style = "color: white; font: 28pt 'TI-92p Mini Sans';"
+        # Hover style with underline
+        self.hover_style = self.default_style + " text-decoration: underline;"
+        # Apply the default style initially
+        self.setStyleSheet(self.default_style)
+
+    def enterEvent(self, event):
+        super().enterEvent(event)
+        # Change the text and apply the hover style on hover
+        self.setText(self.hover_text)
+        self.setStyleSheet(self.hover_style)
+
+    def leaveEvent(self, event):
+        super().leaveEvent(event)
+        # Revert to the default text and style when not hovered
+        self.setText(self.default_text)
+        self.setStyleSheet(self.default_style)
 class Ui_InitConfMain(object):
-    def setupUi(self, InitConfMain):
+    def setupUi(self, InitConfMain, stackedWidget):
         InitConfMain.setObjectName("InitConfMain")
         InitConfMain.resize(1920, 1080)
         InitConfMain.setStyleSheet("background-color: black;")
+        self.stackedWidget = stackedWidget
         self.line_5 = QtWidgets.QFrame(parent=InitConfMain)
         self.line_5.setGeometry(QtCore.QRect(1400, 10, 511, 21))
         self.line_5.setStyleSheet("background-color: gray;")
@@ -44,21 +70,19 @@ class Ui_InitConfMain(object):
         self.label.setFont(font)
         self.label.setStyleSheet("color: gray;")
         self.label.setObjectName("label")
-        self.AdminControlsButton = QtWidgets.QPushButton(parent=InitConfMain)
+
+        self.AdminControlsButton = HoverButton("- Admin Controls", "> Admin Controls", parent=InitConfMain)
         self.AdminControlsButton.setGeometry(QtCore.QRect(80, 80, 431, 41))
-        self.AdminControlsButton.setStyleSheet("color: white;\n"
-"font: 28pt \"TI-92p Mini Sans\";")
         self.AdminControlsButton.setObjectName("AdminControlsButton")
-        self.AddUserButton = QtWidgets.QPushButton(parent=InitConfMain)
+
+        self.AddUserButton = HoverButton("- Add User", "> Add User", parent=InitConfMain)
         self.AddUserButton.setGeometry(QtCore.QRect(80, 160, 281, 41))
-        self.AddUserButton.setStyleSheet("color: white;\n"
-"font: 28pt \"TI-92p Mini Sans\";")
         self.AddUserButton.setObjectName("AddUserButton")
-        self.AccessiblilityButton = QtWidgets.QPushButton(parent=InitConfMain)
+
+        self.AccessiblilityButton = HoverButton("- Accessibility", "> Accessibility", parent=InitConfMain)
         self.AccessiblilityButton.setGeometry(QtCore.QRect(80, 240, 411, 41))
-        self.AccessiblilityButton.setStyleSheet("color: white;\n"
-"font: 28pt \"TI-92p Mini Sans\";")
-        self.AccessiblilityButton.setObjectName("AccessiblilityButton")
+        self.AccessiblilityButton.setObjectName("AccessibilityButton")
+
         self.line.raise_()
         self.line_5.raise_()
         self.line_4.raise_()
@@ -66,14 +90,19 @@ class Ui_InitConfMain(object):
         self.line_2.raise_()
         self.label.raise_()
         self.AdminControlsButton.raise_()
+
         self.AddUserButton.raise_()
+        self.AddUserButton.clicked.connect(self.goToAddUserScreen)
         self.AccessiblilityButton.raise_()
-        self.InitConfScreenOneButton = QtWidgets.QPushButton(parent=InitConfMain)
+        #self.InitConfScreenOneButton = QtWidgets.QPushButton(parent=InitConfMain)
 
         self.retranslateUi(InitConfMain)
         QtCore.QMetaObject.connectSlotsByName(InitConfMain)
-
         #self.button.clicked.connect(self.goToInitConfMain)
+
+    def goToAddUserScreen(self):
+        currentIndex = self.stackedWidget.currentIndex()
+        self.stackedWidget.setCurrentIndex(self.stackedWidget.currentIndex() + 1)
 
     def retranslateUi(self, InitConfMain):
         _translate = QtCore.QCoreApplication.translate
@@ -82,7 +111,6 @@ class Ui_InitConfMain(object):
         self.AdminControlsButton.setText(_translate("InitConfMain", "- Admin Controls"))
         self.AddUserButton.setText(_translate("InitConfMain", "- Add User"))
         self.AccessiblilityButton.setText(_translate("InitConfMain", "- Accessibility"))
-
 
 # if __name__ == "__main__":
 #     import sys
