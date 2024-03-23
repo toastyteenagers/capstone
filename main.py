0719e802fd4db07f7bbc837507aec8ff4372cfce
Roohan Amin <roohan.amin@gmail.com>
2024-03-22 23:42:20 -0700

    added configparsing, needs to scale so that config file populates as updates occur.

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import sys
+import os
 from PyQt6 import QtWidgets, QtCore
 from startup import Ui_LoadScreen
 from initialConfigScreenOne import Ui_InitConfScreenOne
 from mainInitialConfigScreen import Ui_InitConfMain
 from addUserScreen import Ui_addUserScreen
+from configparser import ConfigParser
+
+
 # from userlist import Ui_UserList
 
 class System(QtWidgets.QWidget):
@@ -13,7 +17,7 @@ class System(QtWidgets.QWidget):
 
         self.stackedWidget.setStyleSheet("border: none;")
         layout = QtWidgets.QVBoxLayout(self)
-        layout.setContentsMargins(0,0,0,0)
+        layout.setContentsMargins(0, 0, 0, 0)
         layout.setSpacing(0)
         layout.addWidget(self.stackedWidget)
 
@@ -35,24 +39,26 @@ class System(QtWidgets.QWidget):
         self.ui_initConfScreenOne = Ui_InitConfScreenOne()
         self.ui_initConfScreenOne.setupUi(self.initConfScreenOne, self.stackedWidget)
         self.stackedWidget.addWidget(self.initConfScreenOne)
+
     def setupMainInitialConfigScreen(self):
         self.initConfMain = QtWidgets.QWidget()
         self.ui_initConfMain = Ui_InitConfMain()
         self.ui_initConfMain.setupUi(self.initConfMain, self.stackedWidget)
         self.stackedWidget.addWidget(self.initConfMain)
         self.stackedWidget.setCurrentWidget(self.startupScreen)
+
     def showStartupScreen(self):
         self.stackedWidget.setCurrentWidget(self.startupScreen)
         QtCore.QTimer.singleShot(1000, self.ui_startup.playStartupSound)
         QtCore.QTimer.singleShot(5000, self.showInitialConfigScreen)
 
-    def showInitialConfigScreen(self): # ok button screen
+    def showInitialConfigScreen(self):  # ok button screen
         self.stackedWidget.setCurrentWidget(self.initConfScreenOne)
 
-    def showMainInitialConfigScreen(self): # first time set up steps
+    def showMainInitialConfigScreen(self):  # first time set up steps
         self.stackedWidget.setCurrentWidget(self.initConfMain)
 
-    def setupAddUserScreen(self): # second step
+    def setupAddUserScreen(self):  # second step
         self.addUserScreen = QtWidgets.QWidget()
         self.ui_addUserScreen = Ui_addUserScreen()
         self.ui_addUserScreen.setupUi(self.addUserScreen)
@@ -64,7 +70,9 @@ class System(QtWidgets.QWidget):
 
 
 if __name__ == "__main__":
-    app = QtWidgets.QApplication(sys.argv)
-    programRun = System()
-    programRun.showFullScreen()
-    sys.exit(app.exec())
+    file = 'initial_config.ini'
+    if os.stat(file).st_size == 0:
+        app = QtWidgets.QApplication(sys.argv)
+        programRun = System()
+        programRun.showFullScreen()
+        sys.exit(app.exec())
