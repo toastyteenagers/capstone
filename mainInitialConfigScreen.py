90a2b642de1eeccfd463812506ffd75968e01e5c
Roohan Amin <roohan.amin@gmail.com>
2024-04-21 19:25:33 -0700

    gradient main screen that the user only sees, measures heart rate and detects user

--- mainInitialConfigScreen.py
+++ mainInitialConfigScreen.py
@@ -1,122 +1,118 @@
 from PyQt6 import QtCore, QtGui, QtWidgets
 
-
 class HoverButton(QtWidgets.QPushButton):
     def __init__(self, default_text, hover_text, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.default_text = default_text
         self.hover_text = hover_text
-        # Set the default text initially
         self.setText(self.default_text)
-        # Default style
         self.default_style = "color: white; font: 28pt 'TI-92p Mini Sans';"
-        # Hover style with underline
-        self.hover_style = self.default_style + " text-decoration: underline;"
-        # Apply the default style initially
+        self.hover_style = self.default_style + "text-decoration: underline;"
         self.setStyleSheet(self.default_style)
 
     def enterEvent(self, event):
         super().enterEvent(event)
-        # Change the text and apply the hover style on hover
         self.setText(self.hover_text)
         self.setStyleSheet(self.hover_style)
 
     def leaveEvent(self, event):
         super().leaveEvent(event)
-        # Revert to the default text and style when not hovered
         self.setText(self.default_text)
         self.setStyleSheet(self.default_style)
 class Ui_InitConfMain(object):
-    def setupUi(self, InitConfMain, stackedWidget):
+    def setupUi(self, InitConfMain):
         InitConfMain.setObjectName("InitConfMain")
         InitConfMain.resize(1920, 1080)
         InitConfMain.setStyleSheet("background-color: black;")
-        self.stackedWidget = stackedWidget
-        self.line_5 = QtWidgets.QFrame(parent=InitConfMain)
-        self.line_5.setGeometry(QtCore.QRect(1400, 10, 511, 21))
-        self.line_5.setStyleSheet("background-color: gray;")
-        self.line_5.setFrameShape(QtWidgets.QFrame.Shape.HLine)
-        self.line_5.setFrameShadow(QtWidgets.QFrame.Shadow.Sunken)
-        self.line_5.setObjectName("line_5")
-        self.line_4 = QtWidgets.QFrame(parent=InitConfMain)
-        self.line_4.setGeometry(QtCore.QRect(10, 1020, 1901, 21))
-        self.line_4.setStyleSheet("background-color: gray;")
-        self.line_4.setFrameShape(QtWidgets.QFrame.Shape.HLine)
-        self.line_4.setFrameShadow(QtWidgets.QFrame.Shadow.Sunken)
-        self.line_4.setObjectName("line_4")
-        self.line_3 = QtWidgets.QFrame(parent=InitConfMain)
-        self.line_3.setGeometry(QtCore.QRect(1890, 10, 21, 1031))
-        self.line_3.setStyleSheet("background-color: gray")
-        self.line_3.setFrameShape(QtWidgets.QFrame.Shape.VLine)
-        self.line_3.setFrameShadow(QtWidgets.QFrame.Shadow.Sunken)
-        self.line_3.setObjectName("line_3")
-        self.line_2 = QtWidgets.QFrame(parent=InitConfMain)
-        self.line_2.setGeometry(QtCore.QRect(10, 10, 21, 1031))
-        self.line_2.setStyleSheet("background-color: gray")
-        self.line_2.setFrameShape(QtWidgets.QFrame.Shape.VLine)
-        self.line_2.setFrameShadow(QtWidgets.QFrame.Shadow.Sunken)
-        self.line_2.setObjectName("line_2")
-        self.line = QtWidgets.QFrame(parent=InitConfMain)
-        self.line.setGeometry(QtCore.QRect(10, 10, 521, 21))
-        self.line.setStyleSheet("background-color: gray;")
-        self.line.setFrameShape(QtWidgets.QFrame.Shape.HLine)
-        self.line.setFrameShadow(QtWidgets.QFrame.Shadow.Sunken)
-        self.line.setObjectName("line")
-        self.label = QtWidgets.QLabel(parent=InitConfMain)
-        self.label.setGeometry(QtCore.QRect(550, 10, 831, 51))
+        self.topRightLine = QtWidgets.QFrame(parent=InitConfMain)
+        self.topRightLine.setGeometry(QtCore.QRect(1290, 30, 621, 21))
+        self.topRightLine.setStyleSheet("background-color: gray;")
+        self.topRightLine.setFrameShape(QtWidgets.QFrame.Shape.HLine)
+        self.topRightLine.setFrameShadow(QtWidgets.QFrame.Shadow.Sunken)
+        self.topRightLine.setObjectName("topRightLine")
+        self.bottomLine = QtWidgets.QFrame(parent=InitConfMain)
+        self.bottomLine.setGeometry(QtCore.QRect(10, 1020, 1901, 21))
+        self.bottomLine.setStyleSheet("background-color: gray;")
+        self.bottomLine.setFrameShape(QtWidgets.QFrame.Shape.HLine)
+        self.bottomLine.setFrameShadow(QtWidgets.QFrame.Shadow.Sunken)
+        self.bottomLine.setObjectName("bottomLine")
+        self.rightLine = QtWidgets.QFrame(parent=InitConfMain)
+        self.rightLine.setGeometry(QtCore.QRect(1890, 30, 21, 1011))
+        self.rightLine.setStyleSheet("background-color: gray")
+        self.rightLine.setFrameShape(QtWidgets.QFrame.Shape.VLine)
+        self.rightLine.setFrameShadow(QtWidgets.QFrame.Shadow.Sunken)
+        self.rightLine.setObjectName("rightLine")
+        self.leftLine = QtWidgets.QFrame(parent=InitConfMain)
+        self.leftLine.setGeometry(QtCore.QRect(10, 30, 21, 1011))
+        self.leftLine.setStyleSheet("background-color: gray")
+        self.leftLine.setFrameShape(QtWidgets.QFrame.Shape.VLine)
+        self.leftLine.setFrameShadow(QtWidgets.QFrame.Shadow.Sunken)
+        self.leftLine.setObjectName("leftLine")
+        self.topLeftLine = QtWidgets.QFrame(parent=InitConfMain)
+        self.topLeftLine.setGeometry(QtCore.QRect(10, 30, 621, 21))
+        self.topLeftLine.setStyleSheet("background-color: gray;")
+        self.topLeftLine.setFrameShape(QtWidgets.QFrame.Shape.HLine)
+        self.topLeftLine.setFrameShadow(QtWidgets.QFrame.Shadow.Sunken)
+        self.topLeftLine.setObjectName("topLeftLine")
+        self.InitConfTitle = QtWidgets.QLabel(parent=InitConfMain)
+        self.InitConfTitle.setGeometry(QtCore.QRect(640, 10, 651, 51))
         font = QtGui.QFont()
         font.setFamily("TI-92p Mini Sans")
         font.setPointSize(36)
-        self.label.setFont(font)
-        self.label.setStyleSheet("color: gray;")
-        self.label.setObjectName("label")
-
-        self.AdminControlsButton = HoverButton("- Admin Controls", "> Admin Controls", parent=InitConfMain)
-        self.AdminControlsButton.setGeometry(QtCore.QRect(80, 80, 431, 41))
-        self.AdminControlsButton.setObjectName("AdminControlsButton")
-
-        self.AddUserButton = HoverButton("- Add User", "> Add User", parent=InitConfMain)
-        self.AddUserButton.setGeometry(QtCore.QRect(80, 160, 281, 41))
-        self.AddUserButton.setObjectName("AddUserButton")
-
-        self.AccessiblilityButton = HoverButton("- Accessibility", "> Accessibility", parent=InitConfMain)
-        self.AccessiblilityButton.setGeometry(QtCore.QRect(80, 240, 411, 41))
-        self.AccessiblilityButton.setObjectName("AccessibilityButton")
-
-        self.line.raise_()
-        self.line_5.raise_()
-        self.line_4.raise_()
-        self.line_3.raise_()
-        self.line_2.raise_()
-        self.label.raise_()
+        self.InitConfTitle.setFont(font)
+        self.InitConfTitle.setStyleSheet("color: gray;")
+        self.InitConfTitle.setObjectName("InitConfTitle")
+        self.AdminControlsButton = HoverButton("- Admin Controls", "Admin Controls", parent=InitConfMain)
+        self.AdminControlsButton.setGeometry(QtCore.QRect(90, 80, 321, 41))
+        #self.AdminControlsButton.setStyleSheet("color: white;\n""font: 28pt \"TI-92p Mini Sans\";")
+        #self.AdminControlsButton.setObjectName("AdminControlsButton")
+        self.AddUserButton = HoverButton("- Add User", "Add User", parent=InitConfMain)
+        self.AddUserButton.setGeometry(QtCore.QRect(90, 160, 201, 41))
+        # self.AddUserButton.setStyleSheet("color: white;\n""font: 28pt \"TI-92p Mini Sans\";")
+        # self.AddUserButton.setObjectName("AddUserButton")
+        self.AccessibilityButton = HoverButton("- Accessibility", "Accessibility", parent=InitConfMain)
+        self.AccessibilityButton.setGeometry(QtCore.QRect(90, 240, 261, 41))
+        # self.AccessibilityButton.setStyleSheet("color: white;\n""font: 28pt \"TI-92p Mini Sans\";")
+        # self.AccessibilityButton.setObjectName("AccessibilityButton")
+        self.topLeftLine.raise_()
+        self.topRightLine.raise_()
+        self.bottomLine.raise_()
+        self.rightLine.raise_()
+        self.leftLine.raise_()
+        self.InitConfTitle.raise_()
         self.AdminControlsButton.raise_()
-
         self.AddUserButton.raise_()
-        self.AddUserButton.clicked.connect(self.goToAddUserScreen)
-        self.AccessiblilityButton.raise_()
-        #self.InitConfScreenOneButton = QtWidgets.QPushButton(parent=InitConfMain)
+        self.AccessibilityButton.raise_()
 
         self.retranslateUi(InitConfMain)
         QtCore.QMetaObject.connectSlotsByName(InitConfMain)
-        #self.button.clicked.connect(self.goToInitConfMain)
+
+    def goToAdminControlsScreen(self):
+        currentIndex = self.stackedWidget.currentIndex()
+        self.stackedWidget.setCurrentIndex(self.stackedWidget.currentIndex() + 1)
 
     def goToAddUserScreen(self):
+        currentIndex = self.stackedWidget.currentIndex()
+        self.stackedWidget.setCurrentIndex(self.stackedWidget.currentIndex() + 1)
+
+    def goToAccessibilityScreen(self):
         currentIndex = self.stackedWidget.currentIndex()
         self.stackedWidget.setCurrentIndex(self.stackedWidget.currentIndex() + 1)
 
     def retranslateUi(self, InitConfMain):
         _translate = QtCore.QCoreApplication.translate
         InitConfMain.setWindowTitle(_translate("InitConfMain", "Form"))
-        self.label.setText(_translate("InitConfMain", "First Time Setup Sequence"))
+        self.InitConfTitle.setText(_translate("InitConfMain", "First Time Setup Sequence"))
         self.AdminControlsButton.setText(_translate("InitConfMain", "- Admin Controls"))
         self.AddUserButton.setText(_translate("InitConfMain", "- Add User"))
-        self.AccessiblilityButton.setText(_translate("InitConfMain", "- Accessibility"))
+        self.AccessibilityButton.setText(_translate("InitConfMain", "- Accessibility"))
 
-# if __name__ == "__main__":
-#     import sys
-#     app = QtWidgets.QApplication(sys.argv)
-#     InitConfMain = QtWidgets.QWidget()
-#     ui = Ui_InitConfMain()
-#     ui.setupUi(InitConfMain)
-#     InitConfMain.show()
-#     sys.exit(app.exec())
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    InitConfMain = QtWidgets.QWidget()
+    ui = Ui_InitConfMain()
+    ui.setupUi(InitConfMain)
+    InitConfMain.show()
+    sys.exit(app.exec())
