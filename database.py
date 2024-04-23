2976bf26006758e97498ce76fb93adffba3a8087
Owen6 <owen.box2@gmail.com>
2024-04-22 21:51:17 -0700

    Database implementation using Sqlite3, marshmallow, and json. database.py needs to only ran once before use and never again.

---
+++ database.py
@@ -0,0 +1,30 @@
+#from users import newUser, administrator, createUser
+import sqlite3
+
+#conn = sqlite3.connect(':memory:')
+conn = sqlite3.connect('users.db')
+
+c = conn.cursor()
+
+c.execute("""CREATE TABLE users (
+          name text,
+          encodings text,
+          rhr integer,
+          disability integer,
+          uses text,
+          doc text,
+          level integer
+        )""")
+
+#def add_database(data):
+#    c.execute("INSERT INTO users VALUES (data['name'], data['encodings'], data['rhr'], data['disability'], data['uses'], data['doc'], data['level'])")
+
+#c.execute("INSERT INTO users VALUES ('name', [1234], 90, 0, [], 'today', 1)")
+
+
+#c.execute("INSERT INTO users VALUES ('Owen', [1234], 95, )")
+
+#createUser("Owen", [1234], 95, 0)
+conn.commit()
+
+conn.close()
